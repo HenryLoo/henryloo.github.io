0c858581b30799355a87f4602ee1fc45221914dd
Henry Loo <henry_loo@live.ca>
2017-11-09 14:02:35 -0800

    Implement menu test

--- tipper/assets/js/foodmenus.js
+++ tipper/assets/js/foodmenus.js
@@ -4,8 +4,44 @@
 
         $( ".menu_select" ).change( function() {
 
-            if( $(this).val() )
-                $( ".menu_area" ).load( "parts/menus/menu_"+$(this).val()+".html" );
+            if( $( this ).val() )
+            {
+                //$( ".menu_area" ).load( "parts/menus/menu_"+$(this).val()+".html" );
+
+                var menu = "";
+                menu += '<table class="food_menus menu_1 alt">';
+                menu += '<thead>';
+                menu += '<tr>';
+                menu += '<th>Name</th>';
+                menu += '<th>Description</th>';
+                menu += '<th>Price</th>';
+                menu += '</tr>';
+                menu += '</thead>';
+                menu += '<tbody>';
+
+                $.get( "parts/menus/menu_" + $( this ).val() + ".txt", function( data ) {
+
+                  var line = data.split( "\n" );
+
+                    var i = 0;
+                    $.each( line, function( key, value ){
+
+                        if( i == 0 ) menu += '<tr>';
+
+                        if( i != 3 ) menu += '<td>' + value + '</td>';
+
+                        if( i == 2 ) menu += '</tr>';
+
+                        if( i == 3 ) i = 0;
+                    });
+
+                });
+
+                menu += '<tbody>';
+                menu += '</table>';
+                
+                $( ".menu_area" ).html( menu );
+            }
             else
                 $( ".menu_area" ).empty();
         });
